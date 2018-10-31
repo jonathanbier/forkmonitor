96a18b86366f813ed7b3a9e61d7548c5fc6dee64
Sjors Provoost <sjors@sprovoost.nl>
2018-10-31 16:47:53 +0100

    Add Bootstrap 4 using reactstrap.

--- app/javascript/packs/forkMonitorApp/components/landingPage.jsx
+++ app/javascript/packs/forkMonitorApp/components/landingPage.jsx
@@ -1,11 +1,58 @@
 import React from 'react';
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+    Jumbotron
+} from 'reactstrap';
+
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+        isOpen: false
+    };
+  }
+  toggle() {
+      this.setState({
+          isOpen: !this.state.isOpen
+      });
+  }
   render() {
-    return(
-      <div>
-        <h1>Fork Monitor</h1>
-      </div>
-    )
+      return(
+        <div>
+            <Navbar color="inverse" light expand="md">
+                <NavbarBrand href="/">Fork Monitor</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="https://blog.bitmex.com/research/">BitMEX Research</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+            <Jumbotron>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1>Nodes</h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
+        </div>
+      );
   }
 }
 export default LandingPage
