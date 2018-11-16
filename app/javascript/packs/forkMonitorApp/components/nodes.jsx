3ba9bef0ea9b6484c9c749852bf6de0e529d3682
BitMEXResearch <42411042+BitMEXResearch@users.noreply.github.com>
2018-11-16 14:36:52 +0000

    Update nodes.jsx

--- app/javascript/packs/forkMonitorApp/components/nodes.jsx
+++ app/javascript/packs/forkMonitorApp/components/nodes.jsx
@@ -78,7 +78,7 @@ class Nodes extends React.Component {
           <br />
           { this.state.coin === "bch" &&
             <UncontrolledAlert color="info">
-              The last common block between ABC and SV was mined. Height: 556766, log2(PoW): 87.723, hash: 00000000000000000102d94fde9bd0807a2cc7582fe85dd6349b73ce4e8d9322 at 17:52 UTC on 15th November 2018
+              The last common block between ABC and SV was mined. Height: 556766, Log2(PoW): 87.723, Hash: 00000000000000000102d94fde9bd0807a2cc7582fe85dd6349b73ce4e8d9322 at 17:52 UTC on 15th November 2018
             </UncontrolledAlert>
           }
           <Container>
