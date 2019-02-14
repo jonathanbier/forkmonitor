acf863eaefd8b34cc8582a3f20edfaae72de6a2c
Sjors Provoost <sjors@sprovoost.nl>
2019-02-14 19:01:30 +0100

    Test: check node renders version, IBD and reachability

--- test/javascript/node.test.js
+++ test/javascript/node.test.js
@@ -7,18 +7,58 @@ configure({ adapter: new Adapter() });
 
 import Node from 'forkMonitorApp/components/node';
 
-test('rendered component', () => {
-  const chaintip = {
-    hash: "abcd",
-    height: 500000,
-    timestamp: 1,
-    work: 86.000001
-  }
-  const node = {id: 1, name: "Bitcoin Core", version: 170100, best_block: chaintip, unreachable_since: null, ibd: false}
+const chaintip = {
+  hash: "abcd",
+  height: 500000,
+  timestamp: 1,
+  work: 86.000001
+}
 
-  const wrapper = shallow(<Node
-    key={ 0 }
-    node={ node }
-  />);
-  expect(wrapper.find('.node-version')).toHaveLength(1);
+let wrapper;
+
+describe('Node', () => {
+  const node = {id: 1, name: "Bitcoin Core", version: 170100, best_block: chaintip, unreachable_since: null, ibd: false};
+
+  beforeAll(() => {
+    wrapper = shallow(<Node
+      key={ 0 }
+      node={ node }
+    />)
+  });
+
+  test('should display version', () => {
+    expect(wrapper.find('.node-version')).toHaveLength(1);
+    expect(wrapper.find('.node-version').text()).toContain("0.17.1");
+  });
+});
+
+describe('Sync', () => {
+  const node = {id: 1, name: "Bitcoin Core", version: 170100, best_block: chaintip, unreachable_since: null, ibd: false};
+
+  test('should be indicated', () => {
+    wrapper = shallow(<Node
+      key={ 0 }
+      node={ node }
+    />)
+    expect(wrapper.find("Badge")).toHaveLength(0);
+    node.ibd = true;
+    wrapper = shallow(<Node
+      key={ 0 }
+      node={ node }
+    />)
+    expect(wrapper.find("Badge")).toHaveLength(1);
+  });
+});
+
+describe('Reachability', () => {
+  const node = {id: 1, name: "Bitcoin Core", version: 170100, best_block: chaintip, unreachable_since: null, ibd: false};
+
+  test('should indicate when unreachable', () => {
+    node.unreachable_since = "2019-02-14T17:54:31.959Z";
+    wrapper = shallow(<Node
+      key={ 0 }
+      node={ node }
+    />)
+    expect(wrapper.find("Badge")).toHaveLength(1);
+  });
 });
