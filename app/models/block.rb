ece95570b2c4134b814e47eafcb0c4c3a3654769
Sjors Provoost <sjors@sprovoost.nl>
2019-02-18 21:15:26 +0100

    Reduce info in invalid_blocks.json to prevent infinite recursion

--- app/models/block.rb
+++ app/models/block.rb
@@ -5,7 +5,14 @@ class Block < ApplicationRecord
   belongs_to :first_seen_by, class_name: 'Node', foreign_key: 'first_seen_by_id', optional: true
 
   def as_json(options = nil)
-    super({ only: [:height, :timestamp] }.merge(options || {})).merge({hash: block_hash, work: log2_pow, first_seen_by: first_seen_by})
+    super({ only: [:height, :timestamp] }.merge(options || {})).merge({
+      hash: block_hash,
+      work: log2_pow,
+      first_seen_by: first_seen_by ? {
+        id: first_seen_by.id,
+        name: first_seen_by.name,
+        version: first_seen_by.version
+      } : nil})
   end
 
   def log2_pow
--- app/models/invalid_block.rb
+++ app/models/invalid_block.rb
@@ -3,6 +3,10 @@ class InvalidBlock < ApplicationRecord
   belongs_to :node
 
   def as_json(options = nil)
-    super({ only: [:id] }).merge({block: block, node: node})
+    super({ only: [:id] }).merge({block: block, node: {
+      id: node.id,
+      name: node.name,
+      version: node.version
+    }})
   end
 end
