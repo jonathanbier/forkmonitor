a1cbc5fb48b45eccde3c1f4f4df9961acec81272
Sjors Provoost <sjors@sprovoost.nl>
2019-03-04 12:12:04 +0100

    Enable running one specific test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@
 # Coveralls.wear!
 
 RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
