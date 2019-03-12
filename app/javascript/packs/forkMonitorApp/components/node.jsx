fcd4f30b3764cc66ae7db5d90e08f4929f046526
Sjors Provoost <sjors@sprovoost.nl>
2019-03-12 12:05:30 +0100

    Add online badge, refactor Node component

--- app/javascript/packs/forkMonitorApp/components/node.jsx
+++ app/javascript/packs/forkMonitorApp/components/node.jsx
@@ -11,25 +11,20 @@ import {
 import NodeName from './nodeName';
 
 class Node extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      node: props.node
-    };
-  }
-
   render() {
+    let badge;
+    if (this.props.node.unreachable_since != null) {
+      badge = <Badge color="warning">Offline</Badge>;
+    } else if ( this.props.node.ibd  ) {
+      badge = <Badge color="info">Syncing</Badge>;
+    } else {
+      badge = <Badge color="success">Online</Badge>;
+    }
     return(
       <li>
         <b>
-          <NodeName node={this.state.node} />
-          {this.state.node.unreachable_since!=null &&
-            <span> <Badge color="warning">Offline</Badge></span>
-          }
-          {this.state.node.ibd &&
-            <span> <Badge color="info">Syncing</Badge></span>
-          }
+          <NodeName node={this.props.node} />
+          <span> {badge}</span>
         </b>
       </li>
     )
--- test/javascript/node.test.js
+++ test/javascript/node.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { shallow, configure } from 'enzyme';
+import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
@@ -18,42 +18,33 @@ let wrapper;
 
 describe('Node', () => {
   const node = {id: 1, name: "Bitcoin Core", version: 170100, best_block: chaintip, unreachable_since: null, ibd: false};
+});
+
+describe('Status badge', () => {
+  const node = {id: 1, name: "Bitcoin Core", version: 170100, best_block: chaintip, unreachable_since: null, ibd: false};
+
+  let wrapper;
 
   beforeAll(() => {
-    wrapper = shallow(<Node
+    wrapper = mount(<Node
       key={ 0 }
       node={ node }
     />)
   });
-});
-
-describe('Sync', () => {
-  const node = {id: 1, name: "Bitcoin Core", version: 170100, best_block: chaintip, unreachable_since: null, ibd: false};
-
-  test('should be indicated', () => {
-    wrapper = shallow(<Node
-      key={ 0 }
-      node={ node }
-    />)
-    expect(wrapper.find("Badge")).toHaveLength(0);
-    node.ibd = true;
-    wrapper = shallow(<Node
-      key={ 0 }
-      node={ node }
-    />)
-    expect(wrapper.find("Badge")).toHaveLength(1);
-  });
-});
-
-describe('Reachability', () => {
-  const node = {id: 1, name: "Bitcoin Core", version: 170100, best_block: chaintip, unreachable_since: null, ibd: false};
 
   test('should indicate when unreachable', () => {
-    node.unreachable_since = "2019-02-14T17:54:31.959Z";
-    wrapper = shallow(<Node
-      key={ 0 }
-      node={ node }
-    />)
-    expect(wrapper.find("Badge")).toHaveLength(1);
+    wrapper.setProps({node: {unreachable_since: "2019-02-14T17:54:31.959Z"}});
+    expect(wrapper.find("Badge").text()).toEqual("Offline");
   });
+
+  test('should indicate when online, but syncing', () => {
+    wrapper.setProps({node: {unreachable_since: null, ibd: true}});
+    expect(wrapper.find("Badge").text()).toEqual("Syncing");
+  });
+
+  test('should indicate when online', () => {
+    wrapper.setProps({node: {unreachable_since: null, ibd: false}});
+    expect(wrapper.find("Badge").text()).toEqual("Online");
+  });
+
 });
