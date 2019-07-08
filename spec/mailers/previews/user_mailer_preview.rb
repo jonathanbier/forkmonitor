f8776f5a20bce99493d7c8d7845bb1830029750c
Sjors Provoost <sjors@sprovoost.nl>
2019-07-08 13:17:21 +0100

    List blocks and pools in version bit email

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/nodes_mailer
 class NodesMailerPreview < ActionMailer::Preview
-
+  def version_bits_email
+    @version_bit = VersionBit.last
+    tally = 1 # TODO: store in VersionBit
+    @block = Block.find(@version_bit.activate_block_id)
+    UserMailer.with(user: User.first, bit: @version_bit.bit, tally: 1, window: VersionBit::WINDOW, block: @block).version_bits_email
+  end
 end
