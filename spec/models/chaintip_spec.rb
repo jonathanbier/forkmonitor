jonathanbier/forkmonitor

commit ce85e5346ce42f1bb422795e7f47a2ee70130fbf

Chaintip: match_parent! remove redudant block argument

app/models/chaintip.rb
@@ -1,6 +1,6 @@
 class Chaintip < ApplicationRecord
-  belongs_to :block
-  belongs_to :node
+  belongs_to :block, optional: false
+  belongs_to :node, optional: false
   belongs_to :parent_chaintip, class_name: 'Chaintip', foreign_key: 'parent_chaintip_id', optional: true  # When a node is behind, we assume it would agree with this chaintip, until getchaintips says otherwise
 
   enum coin: [:btc, :bch, :bsv]
@@ -20,7 +20,7 @@ class Chaintip < ApplicationRecord
     super({ only: fields }.merge(options || {})).merge({block: block, nodes: nodes})
   end
 
-  def match_parent!(block, node)
+  def match_parent!(node)
     # Check if any of the other nodes are ahead of us. Use their chaintip instead unless we consider it invalid:
     Chaintip.joins(:block).where(coin: self.coin, status: "active").where("blocks.height > ?", block.height).each do |candidate_tip|
       break if node.chaintips.find_by(block: candidate_tip.block, status: "invalid")
@@ -44,9 +44,12 @@ class Chaintip < ApplicationRecord
       # A block may have arrived between when we called getblockchaininfo and getchaintips.
       # In that case, ignore the new chaintip and get back to it later.
       return nil unless block.present?
-      tip = node.chaintips.find_or_create_by(status: "active", coin: block.coin) # There can only be one
-      tip.update block: block, parent_chaintip: nil
-      tip.match_parent!(block, node)
+      tip = Chaintip.find_or_initialize_by(status: "active", coin: block.coin) # There can only be one
+      tip.node = node if tip.node.nil? # Associate active chaintip with the first node that finds it
+      tip.block = block
+      tip.parent_chaintip = nil
+      tip.save
+      tip.match_parent!(node)
     when "valid-fork"
       return nil if chaintip["height"] < node.block.height - 1000
       block = Block.find_or_create_block_and_ancestors!(chaintip["hash"], node)

app/models/node.rb
@@ -150,7 +150,7 @@ class Node < ApplicationRecord
 
         tip = Chaintip.find_or_create_by(coin: :btc, node: self, status: "active")
         tip.update block: self.block, parent_chaintip: nil
-        tip.match_parent!(self.block, self)
+        tip.match_parent!(self)
         return nil
       end
     end

commit c8d0ff770fca5bf2e04ed7aaab354069abe7f68e

Fix process_active! and add more tests
This regression may have been introduced in ce85e5346ce42f1bb422795e7f47a2ee70130fbf, but it's unclear how.

app/models/chaintip.rb
@@ -9,6 +9,7 @@ class Chaintip < ApplicationRecord
   validates :status, uniqueness: { scope: :node}
 
   def nodes
+    return nil if status != "active"
     res = Node.joins(:chaintips).where("chaintips.block_id = ?", self.block_id).where("chaintips.status = ?", self.status).order(client_type: :asc ,name: :asc, version: :desc).to_a
     Chaintip.where(status: self.status, parent_chaintip: self).each do |child|
       res.append child.node
@@ -98,12 +99,12 @@ class Chaintip < ApplicationRecord
    end
 
    def self.process_active!(node, block)
-     tip = Chaintip.find_or_initialize_by(status: "active", coin: block.coin) # There can only be one
+     tip = Chaintip.find_or_initialize_by(status: "active", coin: block.coin, block: block) # We merge all "active" chaintips for each unique block
      tip.node = node if tip.node.nil? # Associate active chaintip with the first node that finds it
-     tip.block = block
      tip.parent_chaintip = nil
      tip.save
      tip.match_children!(node)
      tip.match_parent!(node)
+     return tip
    end
 end

spec/models/chaintip_spec.rb
@@ -1,6 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Chaintip, type: :model do
+  describe "process_active!" do
+    let(:nodeA) { create(:node) }
+    let(:nodeB) { create(:node) }
+    let(:block1) { create(:block) }
+    let(:block2) { create(:block, parent: block1) }
+    let(:block3) { create(:block, parent: block2) }
+    let(:chaintip1) { create(:chaintip, block: block2, node: nodeA) }
+
+    it "should reuse chaintip for the same block" do
+      chaintip1 # ensure it's instantiated before the next line
+      tip = Chaintip.process_active!(nodeB, block2)
+      expect(tip).to eq(chaintip1)
+    end
+
+    it "should match parent block" do
+      chaintip1
+      tip = Chaintip.process_active!(nodeB, block1)
+      expect(tip).not_to be(chaintip1)
+      expect(tip.parent_chaintip).to eq(chaintip1)
+    end
+
+    it "should match child block" do
+      chaintip1
+      # Feed node B a chaintip that's more recent than the chaintip for node A
+      tip = Chaintip.process_active!(nodeB, block3)
+      expect(tip).not_to be(chaintip1)
+      chaintip1.reload
+      expect(chaintip1.parent_chaintip).to eq(tip)
+    end
+  end
+
+  describe "nodes / process_active!" do
+    let(:nodeA) { create(:node) }
+    let(:nodeB) { create(:node) }
+    let(:block1) { create(:block) }
+    let(:block2) { create(:block, parent: block1) }
+    let(:chaintip1) { create(:chaintip, block: block1, node: nodeA) }
+
+    it "should only support the active chaintip" do
+      chaintip1.update status: "invalid"
+      assert_nil chaintip1.nodes
+    end
+
+    it "should the original node for this active chaintip" do
+      assert_equal chaintip1.nodes, [nodeA]
+    end
+
+    it "should show all nodes at height of active chaintip" do
+      Chaintip.process_active!(nodeB, block1)
+      assert_equal 2, chaintip1.nodes.count
+      assert_equal [nodeB, nodeA], chaintip1.nodes
+    end
+
+    it "should include parent blocks in chaintip" do
+      chaintip1.update block: block2
+      Chaintip.process_active!(nodeB, block1)
+      assert_equal 2, chaintip1.nodes.count
+      assert_equal [nodeA, nodeB], chaintip1.nodes
+    end
+
+  end
+
   describe "match_parent!" do
     let(:nodeA) { create(:node) }
     let(:nodeB) { create(:node) }