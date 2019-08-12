76c617c6f132b44b99cacec7134ff87cd5c53bc9
Sjors Provoost <sjors@sprovoost.nl>
2019-08-12 12:56:40 +0200

    Display pool name
    Fixes #70

--- app/javascript/packs/forkMonitorApp/components/chaintipInfo.jsx
+++ app/javascript/packs/forkMonitorApp/components/chaintipInfo.jsx
@@ -13,6 +13,12 @@ class Chaintip extends React.Component {
       <p className="chaintip-info">
         Height: <NumberFormat value={ this.props.chaintip.block.height } displayType={'text'} thousandSeparator={true} /> (<Moment format="YYYY-MM-DD HH:mm:ss" tz="UTC" parse="X">{this.props.chaintip.block.timestamp}</Moment> UTC)
         <br/>
+        { this.props.chaintip.block.pool &&
+          <span>
+            Mined by: { this.props.chaintip.block.pool } 
+            <br/>
+          </span>
+        }
         Accumulated log2(PoW): <NumberFormat value={this.props.chaintip.block.work} displayType={'text'} decimalScale={6} fixedDecimalScale={true} />
         { this.props.chaintip.block.size &&
             <span><br />Latest blocksize: <NumberFormat value={ this.props.chaintip.block.size / 1000000 } displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale={true} /> MB</span>
