ca11f47b89eaf087839f5d0e5621558991da6380
Sjors Provoost <sjors@sprovoost.nl>
2019-08-12 18:44:41 +0200

    Replace white square with box

--- app/javascript/packs/forkMonitorApp/components/nodeBehindBlocks.jsx
+++ app/javascript/packs/forkMonitorApp/components/nodeBehindBlocks.jsx
@@ -13,12 +13,12 @@ class NodeBehindBlocks extends React.Component {
     if (delta <= 4) {
       let blocks = " ";
       for (let i = 0; i < delta; i++) {
-        blocks += "◻️";
+        blocks += "□ ";
       }
-      return(<span>{ blocks }</span>)
+      return(<span>{ blocks } </span>)
     }
 
-    return(<span> ◻️… ◻️</span>)
+    return(<span> □… □ </span>)
   }
 }
 export default NodeBehindBlocks
