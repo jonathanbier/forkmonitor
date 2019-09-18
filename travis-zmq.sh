33f553831b24f144b9a22d26623fbba2a94477d5
Sjors Provoost <sjors@sprovoost.nl>
2019-09-18 08:36:02 +0200

    Travis: cache ZeroMQ

--- travis-zmq.sh
+++ travis-zmq.sh
@@ -1,8 +1,11 @@
-git clone https://github.com/zeromq/zeromq4-x libzmq
-cd libzmq
-git checkout v4.0.9
-./autogen.sh
-./configure --with-pgm
-make
-sudo make install
 #!/bin/bash
+if [ ! -d "$HOME/libzmq/" ]; then
+  git clone https://github.com/zeromq/zeromq4-x $HOME/libzmq
+  cd $HOME/libzmq
+  git checkout v4.0.9
+  ./autogen.sh
+  ./configure --with-pgm
+  make
+fi
+cd $HOME/libzmq
+sudo make install
