f088e70ed35f706798e790c2e1c7c12f216ac62f
Sjors Provoost <sjors@sprovoost.nl>
2019-10-10 12:15:46 +0200

    TimestampField: support nested properties
    E.g. source=block.timestamp

--- app/javascript/packs/forkMonitorApp/components/timestampField.jsx
+++ app/javascript/packs/forkMonitorApp/components/timestampField.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import get from 'lodash/get';
 import Moment from 'react-moment';
 import 'moment-timezone'
 
-const TimestampField = ({ source, record = {} }) => <Moment format="YYYY-MM-DD HH:mm" tz="UTC" parse="X">{record[source]}</Moment>;
+const TimestampField = ({ source, record = {} }) => <Moment format="YYYY-MM-DD HH:mm" tz="UTC" parse="X">{get(record, source)}</Moment>;
 
 TimestampField.propTypes = {
     label: PropTypes.string,
---
+++ test/javascript/timestampField.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+import TimestampField from 'forkMonitorApp/components/timestampField';
+
+const block = {
+  timestamp: 1567444304
+}
+
+let wrapper;
+
+describe('TimestampField', () => {
+
+  test('should render date', () => {
+    wrapper = mount(<TimestampField
+      record={ {timestamp: block.timestamp} }
+      source="timestamp"
+    />)
+    expect(wrapper.text()).toEqual("2019-09-02 17:11");
+  });
+  
+  test('should render date from nested value', () => {
+    wrapper = mount(<TimestampField
+      record={ {block: block} }
+      source="block.timestamp"
+    />)
+    expect(wrapper.text()).toEqual("2019-09-02 17:11");
+  });
+
+});
