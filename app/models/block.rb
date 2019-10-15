86d871ebf7669508e6d99273805a7ebe1234acd9
Sjors Provoost <sjors@sprovoost.nl>
2019-10-15 17:10:06 +0200

    Check inflation for each block by rolling back the chain
    Fixes #88

--- app/models/block.rb
+++ app/models/block.rb
@@ -239,69 +239,113 @@ class Block < ApplicationRecord
       next unless node.mirror_node? && node.core?
       puts "Check #{ node.coin } inflation for #{ node.name_with_version }..." unless Rails.env.test?
       throw "Node in Initial Blockchain Download" if node.ibd
+      
+      puts "Stop p2p networking to prevent the chain from updating underneath us" unless Rails.env.test?
+      node.mirror_client.setnetworkactive(false)
 
       begin
-        # Update mirror node tip and fetch most recent blocks if needed
-        node.poll_mirror!
-        best_mirror_block = Block.find_by(block_hash: node.mirror_client.getbestblockhash())
-      rescue Bitcoiner::Client::JSONRPCError
-        # Ignore failure
-        puts "Unable to connect to mirror node #{ node.id } #{ node.name_with_version }, skipping inflation check."
-        next
-      end
+        # If anything goes wrong, re-enable the p2p networking and undo invalidateblock before throwing
+        invalidated_block_hashes = []
 
-      # Avoid expensive call if we already have this information for the most recent tip (of the mirror node):
-      if best_mirror_block.present? && TxOutset.find_by(block: best_mirror_block, node: node).present?
-        puts "Already checked #{ node.name_with_version } for current mirror tip" unless Rails.env.test?
-        next
-      end
-
-      puts "Get the total UTXO balance at the mirror tip..." unless Rails.env.test?
-      txoutsetinfo = node.mirror_client.gettxoutsetinfo
-      
-      # Make sure we have all blocks up to the mirror tip
-      block = Block.find_by(block_hash: txoutsetinfo["bestblock"])
-      if block.nil?
-        puts "Latest block #{ txoutsetinfo["bestblock"] } at height #{ txoutsetinfo["height"] } missing in blocks database"
-        next
-      end
-
-      tx_outset = TxOutset.create_with(txouts: txoutsetinfo["txouts"], total_amount: txoutsetinfo["total_amount"]).find_or_create_by(block: block, node: node)
-
-      # Find previous block with txoutsetinfo
-      max_inflation = 0
-      comparison_block = block
-      comparison_tx_outset = nil
-      while true
-        max_inflation += comparison_block.max_inflation / 100000000.0
-        comparison_block = comparison_block.parent
-        if comparison_block.nil?
-          puts "Unable to check inflation due to missing intermediate block" unless Rails.env.test?
-          break
+        begin
+          # Update mirror node tip and fetch most recent blocks if needed
+          node.poll_mirror!
+          best_mirror_block = Block.find_by(block_hash: node.mirror_client.getbestblockhash())
+        rescue Bitcoiner::Client::JSONRPCError
+          # Ignore failure
+          puts "Unable to connect to mirror node #{ node.id } #{ node.name_with_version }, skipping inflation check."
+          next
         end
-        comparison_tx_outset = TxOutset.find_by(node: node, block: comparison_block)
-        break if comparison_tx_outset.present?
-      end
-      next if comparison_block.nil?
 
-      # Check that inflation does not exceed the maximum permitted miner award per block
-      inflation = tx_outset.total_amount - comparison_tx_outset.total_amount
-      if inflation > max_inflation
-        tx_outset.update inflated: true
-        inflated_block = block.inflated_block || block.create_inflated_block(node: node,comparison_block: comparison_block, max_inflation: max_inflation, actual_inflation: inflation)
-        if !inflated_block.notified_at
-          User.all.each do |user|
-            UserMailer.with(user: user, inflated_block: inflated_block).inflated_block_email.deliver
+        # Avoid expensive call if we already have this information for the most recent tip (of the mirror node):
+        if best_mirror_block.present? && TxOutset.find_by(block: best_mirror_block, node: node).present?
+          puts "Already checked #{ node.name_with_version } for current mirror tip" unless Rails.env.test?
+          next
+        end
+        
+        # We want to call gettxoutsetinfo at every height since the last check.
+        # Roll back the chain using invalidateblock (height + 1) if needed.
+        blocks_to_check = [best_mirror_block]
+        # Find previous block with txoutsetinfo
+        comparison_block = best_mirror_block
+        comparison_tx_outset = nil
+        while true
+          comparison_block = comparison_block.parent
+          if comparison_block.nil?
+            puts "Unable to check inflation due to missing intermediate block" unless Rails.env.test?
+            break
           end
-          inflated_block.update notified_at: Time.now
-          Subscription.blast("inflated-block-#{ inflated_block.id }",
-                             "#{ inflated_block.actual_inflation -  inflated_block.max_inflation } BTC inflation",
-                             "Unexpected #{ inflated_block.actual_inflation -  inflated_block.max_inflation } BTC extra inflation \
-                             between block height #{ inflated_block.comparison_block.height } and #{ inflated_block.block.height } according to #{ node.name_with_version }.",
-          )
+          comparison_tx_outset = TxOutset.find_by(node: node, block: comparison_block)
+          break if comparison_tx_outset.present?
+          # Don't try to calculate inflation for more than 10 blocks; it will take too long to catch up
+          break if best_mirror_block.height - comparison_block.height > 10
+          blocks_to_check.unshift(comparison_block)
         end
-      end
-      # TODO: Process each block and calculate inflation; compare with snapshot.
+                
+        blocks_to_check.each do |block|          
+          if block.height != best_mirror_block.height
+            puts "Roll back the chain to #{ block.height }..." unless Rails.env.test?
+            block.children.each do |child_block|
+              invalidated_block_hashes.append(child_block.block_hash)
+              node.mirror_client.invalidateblock(child_block.block_hash) # This is a blocking call
+            end
+          end
+
+          puts "Get the total UTXO balance at height #{ block.height }..." unless Rails.env.test?
+          txoutsetinfo = node.mirror_client.gettxoutsetinfo
+          
+          unless invalidated_block_hashes.empty?
+            puts "Restore chain to tip..." unless Rails.env.test?
+            invalidated_block_hashes.each do |block_hash|
+              node.mirror_client.reconsiderblock(block_hash) # This is a blocking call
+            end
+            invalidated_block_hashes = []
+          end
+           
+          # Make sure we got the block we expected
+          throw "TxOutset is not for block #{ block.block_hash }" unless txoutsetinfo["bestblock"] == block.block_hash
+          
+          tx_outset = TxOutset.create_with(txouts: txoutsetinfo["txouts"], total_amount: txoutsetinfo["total_amount"]).find_or_create_by(block: block, node: node)
+                
+          # Check that inflation does not exceed the maximum permitted miner award per block
+          prev_tx_outset = TxOutset.find_by(node: node, block: block.parent)
+          if prev_tx_outset.nil?
+            puts "No previous TxOutset to compare against, skipping inflation check for height #{ block.height }..." unless Rails.env.test?
+            next
+          end
+
+          inflation = tx_outset.total_amount - prev_tx_outset.total_amount
+          
+          if inflation > block.max_inflation / 100000000.0
+            tx_outset.update inflated: true
+            inflated_block = block.inflated_block || block.create_inflated_block(node: node,comparison_block: comparison_block, max_inflation: block.max_inflation  / 100000000.0, actual_inflation: inflation)
+            if !inflated_block.notified_at
+              User.all.each do |user|
+                UserMailer.with(user: user, inflated_block: inflated_block).inflated_block_email.deliver
+              end
+              inflated_block.update notified_at: Time.now
+              Subscription.blast("inflated-block-#{ inflated_block.id }",
+                                 "#{ inflated_block.actual_inflation -  inflated_block.max_inflation } BTC inflation",
+                                 "Unexpected #{ inflated_block.actual_inflation -  inflated_block.max_inflation } BTC extra inflation \
+                                 between block height #{ inflated_block.comparison_block.height } and #{ inflated_block.block.height } according to #{ node.name_with_version }.",
+              )
+            end
+          end
+        end
+      rescue
+        puts "Something went wrong, restoring node before bailing out..."
+        puts "Resume p2p networking..."
+        node.mirror_client.setnetworkactive(true)
+        # Have node return to tip
+        invalidated_block_hashes.each do |block_hash|
+          puts "Reconsider block #{ block_hash }"
+          node.mirror_client.reconsiderblock(block_hash) # This is a blocking call
+        end
+        puts "Node restored"
+        raise # continue throwing error
+      end      
+      # Resume p2p networking
+      node.mirror_client.setnetworkactive(true)
     end
   end
 
--- spec/models/inflated_block_spec.rb
+++ spec/models/inflated_block_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe InflatedBlock, type: :model do
       allow(Node).to receive(:where).with(coin: "BTC").and_return [@node_without_mirror, @node]
       allow(Node).to receive(:where).with(coin: "TBTC").and_return [@node_testnet]
     end
+    
+    it "should stop p2p networking and restart it after" do
+      expect(@node.mirror_client).to receive("setnetworkactive").with(false)
+      expect(@node.mirror_client).to receive("setnetworkactive").with(true)
+      Block.check_inflation!(:btc)
+    end
 
     it "should call gettxoutsetinfo on BTC mirror node" do
       expect(@node.mirror_client).to receive("gettxoutsetinfo").and_call_original
@@ -52,30 +58,35 @@ RSpec.describe InflatedBlock, type: :model do
       expect(TxOutset.count).to eq(1)
     end
 
-    describe "BTC mirror node has two more blocks" do
+    describe "BTC mirror node has three more blocks" do
       before do
         Block.check_inflation!(:btc)
-
-        @node.mirror_client.mock_set_height(560178)
+        @node.mirror_client.mock_set_height(560179)
       end
 
       it "should fetch intermediate BTC blocks" do
         Block.check_inflation!(:btc)
-        expect(Block.maximum(:height)).to eq(560178)
-        expect(TxOutset.count).to eq(2)
-        expect(TxOutset.last.block.height).to eq(560178)
+        expect(Block.maximum(:height)).to eq(560179)
+        expect(Block.find_by(height: 560178)).not_to be_nil
+        expect(Block.find_by(height: 560177)).not_to be_nil
+      end
+      
+      it "should invalidate the second block, and later the third block to wind back the tip" do
+        expect(@node.mirror_client).to receive("invalidateblock").with("00000000000000000016816bd3f4da655a4d1fd326a3313fa086c2e337e854f9").ordered.and_call_original
+        expect(@node.mirror_client).to receive("reconsiderblock").with("00000000000000000016816bd3f4da655a4d1fd326a3313fa086c2e337e854f9").ordered.and_call_original
+        expect(@node.mirror_client).to receive("invalidateblock").with("000000000000000000017b592e9ecd6ce8ab9b5a2f391e21ee2e80b022a7dafc").ordered.and_call_original
+        expect(@node.mirror_client).to receive("reconsiderblock").with("000000000000000000017b592e9ecd6ce8ab9b5a2f391e21ee2e80b022a7dafc").ordered.and_call_original
+        Block.check_inflation!(:btc)
       end
 
-      describe "with normal inflation" do
-        before do
-          Block.check_inflation!(:btc)
-        end
-
-        it "mock UTXO set should have increase by be 2 x 12.5 BTC" do
-          expect(TxOutset.last.total_amount - TxOutset.first.total_amount).to eq(25.0)
-        end
-
+      it "should create three new TxOutset entries" do
+        Block.check_inflation!(:btc)
+        expect(TxOutset.count).to eq(4)
+        expect(TxOutset.fourth.total_amount - TxOutset.third.total_amount).to eq(12.5)
+        expect(TxOutset.third.total_amount - TxOutset.second.total_amount).to eq(12.5)
+        expect(TxOutset.second.total_amount - TxOutset.first.total_amount).to eq(12.5)
       end
+
     end
 
     describe "with extra inflation" do
