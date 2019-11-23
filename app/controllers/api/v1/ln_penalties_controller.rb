484d66d30e3261f51f8c014233622eda5eaae748
Sjors Provoost <sjors@sprovoost.nl>
2019-11-23 18:44:07 +0100

    Show most recent penalty first

--- app/controllers/api/v1/ln_penalties_controller.rb
+++ app/controllers/api/v1/ln_penalties_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::LnPenaltiesController < ApplicationController
 
   def index
-    @ln_penalties = LightningTransaction.all
+    @ln_penalties = LightningTransaction.order(created_at: :desc)
     render json: @ln_penalties
   end
 
