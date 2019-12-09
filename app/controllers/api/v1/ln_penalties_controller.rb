d2b5604b202fdcb811c18e7b96281e09d25b26a4
Sjors Provoost <sjors@sprovoost.nl>
2019-12-09 21:15:05 +0100

    Sort justice by block height

--- app/controllers/api/v1/ln_penalties_controller.rb
+++ app/controllers/api/v1/ln_penalties_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::LnPenaltiesController < ApplicationController
 
   def index
-    @ln_penalties = LightningTransaction.order(created_at: :desc)
+    @ln_penalties = LightningTransaction.joins(:block).order(height: :desc)
     render json: @ln_penalties
   end
 
