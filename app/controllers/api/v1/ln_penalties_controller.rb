adce2072828f84634277cbc74e81edf2ffdffe11
Sjors Provoost <sjors@sprovoost.nl>
2020-01-11 15:51:48 +0800

    Fix: class name

--- app/controllers/api/v1/ln_penalties_controller.rb
+++ app/controllers/api/v1/ln_penalties_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::LnPenaltiesController < ApplicationController
 
   def index
-    latest = LightningPenalty.last_updated_cached
+    latest = PenaltyTransaction.last_updated_cached
     if stale?(etag: latest.try(:updated_at), last_modified: latest.try(:updated_at), public: true)
       render json: Rails.cache.fetch('api/v1/ln_penalties.json') {
-        LightningPenalty.all_with_block_cached.to_json
+        PenaltyTransaction.all_with_block_cached.to_json
       }
     end
   end
--- app/controllers/api/v1/ln_stats_controller.rb
+++ app/controllers/api/v1/ln_stats_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::LnStatsController < ApplicationController
 
   def index
-    latest = LightningTransaction.order(updated_at: :desc).first
+    latest = PenaltyTransaction.order(updated_at: :desc).first
     if stale?(etag: latest.try(:updated_at), last_modified: latest.try(:updated_at), public: true)
       @ln_stats = {
-        count: LightningTransaction.count,
-        total: LightningTransaction.sum(:amount)
+        count: PenaltyTransaction.count,
+        total: PenaltyTransaction.sum(:amount)
       }
       render json: @ln_stats
     end
