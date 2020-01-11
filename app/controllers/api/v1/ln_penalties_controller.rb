400e94368aaba617c70ec19790e497c6dfe8372f
Sjors Provoost <sjors@sprovoost.nl>
2020-01-11 15:28:44 +0800

    Fix: don't show all lightning transactions in penalties tab

--- app/controllers/api/v1/ln_penalties_controller.rb
+++ app/controllers/api/v1/ln_penalties_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::LnPenaltiesController < ApplicationController
 
   def index
-    latest = LightningTransaction.last_updated_cached
+    latest = LightningPenalty.last_updated_cached
     if stale?(etag: latest.try(:updated_at), last_modified: latest.try(:updated_at), public: true)
       render json: Rails.cache.fetch('api/v1/ln_penalties.json') {
-        LightningTransaction.all_with_block_cached.to_json
+        LightningPenalty.all_with_block_cached.to_json
       }
     end
   end
