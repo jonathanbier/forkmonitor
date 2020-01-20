574ff140f10d7e3ae9d830e75ed814a592571d82
Sjors Provoost <sjors@sprovoost.nl>
2020-01-20 11:29:25 +0100

    Lightning: match if penalty is not in the first input

--- app/models/penalty_transaction.rb
+++ app/models/penalty_transaction.rb
@@ -12,39 +12,39 @@ class PenaltyTransaction < LightningTransaction
     parsed_block.transactions.each do |tx|
       tx.in.each_with_index do |tx_in, input|
         # Must have a witness
-        break if tx_in.script_witness.empty?
+        next if tx_in.script_witness.empty?
         # Witness must have the correct number of elements
-        break unless tx_in.script_witness.stack.length == 3
+        next unless tx_in.script_witness.stack.length == 3
         signature, flag, toLocalScript = tx_in.script_witness.stack
         # Signature must be DER encoded
-        break unless Bitcoin::Script.is_der_signature?(signature)
+        next unless Bitcoin::Script.is_der_signature?(signature)
         # Script path must be one of justice (non zero switches to OP_IF)
-        break unless flag.unpack('H*')[0] == "01"
+        next unless flag.unpack('H*')[0] == "01"
 
         script = Bitcoin::Script.new(toLocalScript)
         # Witness script must match expected pattern (BOLT #3)
         chunks = script.chunks
-        break unless chunks.length == 9
+        next unless chunks.length == 9
         # OP_IF
-        break unless chunks.shift() == Bitcoin::Script::OP_IF
+        next unless chunks.shift() == Bitcoin::Script::OP_IF
         #  <revocationpubkey>
-        break unless Bitcoin::Script.check_pubkey_encoding?(chunks.shift())
+        next unless Bitcoin::Script.check_pubkey_encoding?(chunks.shift())
         #  OP_ELSE
-        break unless chunks.shift() == Bitcoin::Script::OP_ELSE
+        next unless chunks.shift() == Bitcoin::Script::OP_ELSE
         #      `to_self_delay`
-        break if chunks.shift().instance_of? Bitcoin::Script
+        next if chunks.shift().instance_of? Bitcoin::Script
         #      OP_CHECKSEQUENCEVERIFY
-        break unless chunks.shift() == Bitcoin::Script::OP_NOP3
+        next unless chunks.shift() == Bitcoin::Script::OP_NOP3
         #      OP_DROP
-        break unless chunks.shift() == Bitcoin::Script::OP_DROP
+        next unless chunks.shift() == Bitcoin::Script::OP_DROP
         #      <local_delayedpubkey>
-        break unless Bitcoin::Script.check_pubkey_encoding?(chunks.shift())
+        next unless Bitcoin::Script.check_pubkey_encoding?(chunks.shift())
         #  OP_ENDIF
-        break unless chunks.shift() == Bitcoin::Script::OP_ENDIF
+        next unless chunks.shift() == Bitcoin::Script::OP_ENDIF
         #  OP_CHECKSIG
-        break unless chunks.shift() == Bitcoin::Script::OP_CHECKSIG
+        next unless chunks.shift() == Bitcoin::Script::OP_CHECKSIG
 
-        break unless PenaltyTransaction.where(tx_id: tx.hash, input: input).count == 0
+        next unless PenaltyTransaction.where(tx_id: tx.hash, input: input).count == 0
 
         puts "Penalty: #{ tx.hash }" unless Rails.env.test?
 
