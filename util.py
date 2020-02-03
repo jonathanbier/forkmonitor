48982a2c526f29e09301672d9ba3a4c2bb8d2930
Sjors Provoost <sjors@sprovoost.nl>
2020-02-03 14:45:55 +0100

    Drop TestWrapper Singleton
    It doesn't jive well with pycall.rb

--- util.py
+++ util.py
@@ -8,97 +8,68 @@ SOURCE_DIRECTORY = os.path.dirname(os.path.abspath(__file__)) + "/vendor/bitcoin
 sys.path.append(SOURCE_DIRECTORY + "/test/functional")
 from test_framework.test_framework import BitcoinTestFramework
 
-class TestWrapper:
-    """Singleton TestWrapper class.
-    This wraps the actual TestWrapper class to ensure that users only ever
-    instantiate a single TestWrapper."""
+class TestWrapper(BitcoinTestFramework):
+    """Wrapper Class for BitcoinTestFramework.
+    Provides the BitcoinTestFramework rpc & daemon process management
+    functionality to external python projects."""
 
-    class __TestWrapper(BitcoinTestFramework):
-        """Wrapper Class for BitcoinTestFramework.
-        Provides the BitcoinTestFramework rpc & daemon process management
-        functionality to external python projects."""
+    def set_test_params(self):
+        # This can be overriden in setup() parameter.
+        self.num_nodes = 1
 
-        def set_test_params(self):
-            # This can be overriden in setup() parameter.
-            self.num_nodes = 1
+    def run_test(self):
+        pass
 
-        def run_test(self):
-            pass
+    def setup(self,
+              bitcoind=os.path.abspath(SOURCE_DIRECTORY + "/src/bitcoind"),
+              bitcoincli=None,
+              setup_clean_chain=True,
+              num_nodes=1,
+              network_thread=None,
+              rpc_timeout=60,
+              supports_cli=False,
+              bind_to_localhost_only=True,
+              nocleanup=False,
+              noshutdown=False,
+              cachedir=os.path.abspath(SOURCE_DIRECTORY + "/test/cache"),
+              tmpdir=None,
+              loglevel='INFO',
+              trace_rpc=False,
+              port_seed=os.getpid(),
+              coveragedir=None,
+              configfile=os.path.abspath(SOURCE_DIRECTORY + "/test/config.ini"),
+              pdbonfailure=False,
+              usecli=False,
+              perf=False,
+              randomseed=None):
 
-        def setup(self,
-                  bitcoind=os.path.abspath(SOURCE_DIRECTORY + "/src/bitcoind"),
-                  bitcoincli=None,
-                  setup_clean_chain=True,
-                  num_nodes=1,
-                  network_thread=None,
-                  rpc_timeout=60,
-                  supports_cli=False,
-                  bind_to_localhost_only=True,
-                  nocleanup=False,
-                  noshutdown=False,
-                  cachedir=os.path.abspath(SOURCE_DIRECTORY + "/test/cache"),
-                  tmpdir=None,
-                  loglevel='INFO',
-                  trace_rpc=False,
-                  port_seed=os.getpid(),
-                  coveragedir=None,
-                  configfile=os.path.abspath(SOURCE_DIRECTORY + "/test/config.ini"),
-                  pdbonfailure=False,
-                  usecli=False,
-                  perf=False,
-                  randomseed=None):
+        self.setup_clean_chain = setup_clean_chain
+        self.num_nodes = num_nodes
+        self.network_thread = network_thread
+        self.rpc_timeout = rpc_timeout
+        self.supports_cli = supports_cli
+        self.bind_to_localhost_only = bind_to_localhost_only
 
-            if self.running:
-                print("TestWrapper is already running!")
-                return
+        self.options = argparse.Namespace
+        self.options.nocleanup = nocleanup
+        self.options.noshutdown = noshutdown
+        self.options.cachedir = cachedir
+        self.options.tmpdir = tmpdir
+        self.options.loglevel = loglevel
+        self.options.trace_rpc = trace_rpc
+        self.options.port_seed = port_seed
+        self.options.coveragedir = coveragedir
+        self.options.configfile = configfile
+        self.options.pdbonfailure = pdbonfailure
+        self.options.usecli = usecli
+        self.options.perf = perf
+        self.options.randomseed = randomseed
+        self.options.valgrind = False
 
-            self.setup_clean_chain = setup_clean_chain
-            self.num_nodes = num_nodes
-            self.network_thread = network_thread
-            self.rpc_timeout = rpc_timeout
-            self.supports_cli = supports_cli
-            self.bind_to_localhost_only = bind_to_localhost_only
+        self.options.bitcoind = bitcoind
+        self.options.bitcoincli = bitcoincli
 
-            self.options = argparse.Namespace
-            self.options.nocleanup = nocleanup
-            self.options.noshutdown = noshutdown
-            self.options.cachedir = cachedir
-            self.options.tmpdir = tmpdir
-            self.options.loglevel = loglevel
-            self.options.trace_rpc = trace_rpc
-            self.options.port_seed = port_seed
-            self.options.coveragedir = coveragedir
-            self.options.configfile = configfile
-            self.options.pdbonfailure = pdbonfailure
-            self.options.usecli = usecli
-            self.options.perf = perf
-            self.options.randomseed = randomseed
-            self.options.valgrind = False
+        super().setup()
 
-            self.options.bitcoind = bitcoind
-            self.options.bitcoincli = bitcoincli
-
-            super().setup()
-
-            self.running = True
-
-        def shutdown(self):
-            if not self.running:
-                print("TestWrapper is not running!")
-            else:
-                super().shutdown()
-                self.running = False
-
-    instance = None
-
-    def __new__(cls):
-        if not TestWrapper.instance:
-            TestWrapper.instance = TestWrapper.__TestWrapper()
-            TestWrapper.instance.running = False
-        return TestWrapper.instance
-
-    def __getattr__(self, name):
-        return getattr(self.instance, name)
-
-    def __setattr__(self, name):
-        return setattr(self.instance, name)
+    def shutdown(self):
+        super().shutdown()
