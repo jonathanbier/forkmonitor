ddc767854f3096ab5ccc57062bc5c1750a305078
Sjors Provoost <sjors@sprovoost.nl>
2020-03-03 09:22:38 +0100

    Fail specs early
    Regtest tests are quite slow, so don't wait for all of them to finish running if there's an error.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,8 @@ require 'helpers/controller_spec_helpers.rb'
 RSpec.configure do |config|
   config.filter_run_when_matching :focus
 
+  config.fail_fast = true
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
