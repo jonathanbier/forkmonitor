cdfb274c6084780254f2eef2809f55ddb3bf169f
Sjors Provoost <sjors@sprovoost.nl>
2020-04-08 18:08:20 +0200

    test: remove stray fdescribe

--- spec/models/inflated_block_spec.rb
+++ spec/models/inflated_block_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe InflatedBlock, type: :model do
     end
   end
 
-  fdescribe "InflatedBlock.check_inflation!" do
+  describe "InflatedBlock.check_inflation!" do
     before do
       setup_python_nodes()
 
