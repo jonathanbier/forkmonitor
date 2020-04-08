3efaf84b215dcafd92504c492d40640836c43616
Sjors Provoost <sjors@sprovoost.nl>
2020-04-08 18:40:21 +0200

    Turn off active_storage
    It's unused and Heroku complains about it.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
