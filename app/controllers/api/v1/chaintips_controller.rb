8fd0dbbb8096525230d8ac6cc59da596771e561b
Sjors Provoost <sjors@sprovoost.nl>
2020-04-20 15:06:50 +0200

    ChaintipsController: use @coin instead of param

--- app/controllers/api/v1/chaintips_controller.rb
+++ app/controllers/api/v1/chaintips_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ChaintipsController < ApplicationController
     latest = Chaintip.where(coin: @coin).order(updated_at: :desc).first
     if stale?(etag: latest.try(:updated_at), last_modified: latest.try(:updated_at))
       render json: Rails.cache.fetch("Chaintip.#{params[:coin].downcase}.index.json") {
-        @chaintips = Chaintip.where(status: "active", coin: params[:coin].downcase, parent_chaintip: nil).includes(:block).order("blocks.work desc", "parent_chaintip_id desc")
+        @chaintips = Chaintip.where(status: "active", coin: @coin, parent_chaintip: nil).includes(:block).order("blocks.work desc", "parent_chaintip_id desc")
         @chaintips.uniq{ |s| s.block.block_hash }
       }
     end
