7628991c2bd8163244ecce83f78849a670211fa9
Sjors Provoost <sjors@sprovoost.nl>
2020-06-04 19:12:09 +0200

    Redirect www to root domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
+
   devise_for :users,
              path: '',
              path_names: {
