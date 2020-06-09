2ddf1f79cc3417a4c20f46db65970ae4ed8be994
Sjors Provoost <sjors@sprovoost.nl>
2020-06-09 20:23:41 +0200

    Cache sprocket assets
    Alternative to cc6d5fbe1a5462cebf6b72d5e7e682186aafc486

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, "/home/forkmonitor/#{fetch :application}"
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/cache/assets/sprockets','vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
