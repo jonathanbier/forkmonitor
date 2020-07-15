3689f830c16d670cafff39f78c364ec449ab6ced
Sjors Provoost <sjors@sprovoost.nl>
2020-07-15 14:42:01 +0200

    Reformat Webpush.payload_send

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,18 @@ class Subscription < ApplicationRecord
   def self.blast(tag, subject, body)
     Subscription.all.each do |s|
       begin
-        Webpush.payload_send(endpoint: s.endpoint, message: "#{tag}|#{subject}|#{body}", p256dh: s.p256dh, auth: s.auth, vapid: { subject: "mailto:" + ENV['VAPID_CONTACT_EMAIL'], public_key: ENV['VAPID_PUBLIC_KEY'] , private_key: ENV['VAPID_PRIVATE_KEY'] }, ttl: 60 * 60)
+        Webpush.payload_send(
+          endpoint: s.endpoint,
+          message: "#{tag}|#{subject}|#{body}",
+          p256dh: s.p256dh,
+          auth: s.auth,
+          ttl: 60 * 60,
+          vapid: {
+            subject: "mailto:" + ENV['VAPID_CONTACT_EMAIL'],
+            public_key: ENV['VAPID_PUBLIC_KEY'] ,
+            private_key: ENV['VAPID_PRIVATE_KEY']
+          }
+        )
       rescue Webpush::Unauthorized
         s.destroy
       rescue Webpush::ExpiredSubscription
