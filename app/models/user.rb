fedf4d4fd3bcfa041213a36caeefc9ded8ead64f
Sjors Provoost <sjors@sprovoost.nl>
2020-12-16 12:21:58 +0100

    Handle upstream politics in JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # registerable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :confirmable, :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :confirmable, :jwt_authenticatable, jwt_revocation_strategy: JWTDenylist
 end
