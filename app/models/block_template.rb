e3be8edd69d9fcc2cb842c8bc73e7ea89244e595
Sjors Provoost <sjors@sprovoost.nl>
2021-02-03 22:14:49 +0100

    Allow empty parent block
    Reduce the maximum wait to 5 seconds.
    
    We encountered a situation where the parent block could not be found for 10 minutes. This may indicate a problem with polling, but it's not worth delaying the getblocktemplate calls.

--- app/models/block_template.rb
+++ app/models/block_template.rb
@@ -1,5 +1,5 @@
 class BlockTemplate < ApplicationRecord
-  belongs_to :parent_block, class_name: 'Block'
+  belongs_to :parent_block, class_name: 'Block', optional: true
   belongs_to :node
 
   def self.create_with(node, template)
@@ -11,9 +11,8 @@ class BlockTemplate < ApplicationRecord
       Rails.logger.warn "Parent block #{ template["previousblockhash"] } not found yet..."
       retries += 1
       sleep 1
-      retry if retries < 20
-      # This can only happen if we failed to poll the node in 20 seconds
-      raise "Parent block #{ template["previousblockhash"] } not found after 20 seconds"
+      retry if retries < 5
+      Rails.logger.warn "Parent block #{ template["previousblockhash"] } not found after 5 seconds"
     end
     self.create!(
       height: template["height"],
