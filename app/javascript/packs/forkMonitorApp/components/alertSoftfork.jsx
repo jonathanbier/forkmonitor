7c841443eb875f1ecbb4811e8b21e2319dc14776
Sjors Provoost <sjors@sprovoost.nl>
2021-05-02 16:22:02 +0200

    Change softfork alert from warning to info

--- app/javascript/packs/forkMonitorApp/components/alertSoftfork.jsx
+++ app/javascript/packs/forkMonitorApp/components/alertSoftfork.jsx
@@ -9,8 +9,7 @@ import {
 class AlertSoftfork extends React.Component {
   render() {
     return(
-      <UncontrolledAlert color="warning">
-        status became { this.props.softfork.status } at height { this.props.softfork.height }&nbsp;
+      <UncontrolledAlert color="info">
         { this.props.softfork.fork_type } { this.props.softfork.name } softfork
         status became { this.props.softfork.status } at height { this.props.softfork.height }
         according to { this.props.softfork.node_name }
