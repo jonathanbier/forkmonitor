209df2cf7770eaae6f050b82f636ddd7eb15d29a
Sjors Provoost <sjors@sprovoost.nl>
2023-04-14 13:35:38 +0200

    Delete Transaction entries before blocks

--- db/migrate/20230413185223_drop_testnet.rb
+++ db/migrate/20230413185223_drop_testnet.rb
@@ -5,6 +5,8 @@ class DropTestnet < ActiveRecord::Migration[6.1]
     Softfork.where(coin: 3).destroy_all
     StaleCandidate.where(coin: 3).destroy_all
     TxOutset.joins(:node).where("nodes.coin = ?", 3).destroy_all
+    Transaction.joins(:block).where("coin = ?", 3).delete_all
+    Node.where(coin: 3).update block:nil, mirror_block: nil
     Block.where(coin: 3).delete_all
     Node.where(coin: 3).destroy_all
   end
