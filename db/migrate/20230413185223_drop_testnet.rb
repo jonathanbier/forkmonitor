327fd440783ca065fff8b524b9bac0c1dc16cf27
Sjors Provoost <sjors@sprovoost.nl>
2023-04-14 12:22:43 +0200

    Use delete_all to speed up migration

--- db/migrate/20230413185223_drop_testnet.rb
+++ db/migrate/20230413185223_drop_testnet.rb
@@ -1,10 +1,10 @@
 class DropTestnet < ActiveRecord::Migration[6.1]
   def up
-    BlockTemplate.where(coin: 3).destroy_all
-    Chaintip.where(coin: 3).destroy_all
-    Softfork.where(coin: 3).destroy_all
-    StaleCandidate.where(coin: 3).destroy_all
-    Block.where(coin: 3).destroy_all
-    Node.where(coin: 3).destroy_all
+    BlockTemplate.where(coin: 3).delete_all
+    Chaintip.where(coin: 3).delete_all
+    Softfork.where(coin: 3).delete_all
+    StaleCandidate.where(coin: 3).delete_all
+    Block.where(coin: 3).delete_all
+    Node.where(coin: 3).delete_all
   end
 end
