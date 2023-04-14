68891415bf0a3afe679045eb010bafc03bc38cee
Sjors Provoost <sjors@sprovoost.nl>
2023-04-14 13:05:17 +0200

    Delete testnet txoutsets

--- db/migrate/20230413185223_drop_testnet.rb
+++ db/migrate/20230413185223_drop_testnet.rb
@@ -4,6 +4,7 @@ class DropTestnet < ActiveRecord::Migration[6.1]
     Chaintip.where(coin: 3).destroy_all
     Softfork.where(coin: 3).destroy_all
     StaleCandidate.where(coin: 3).destroy_all
+    TxOutset.joins(:node).where("nodes.coin = ?", 3).destroy_all
     Block.where(coin: 3).delete_all
     Node.where(coin: 3).destroy_all
   end
