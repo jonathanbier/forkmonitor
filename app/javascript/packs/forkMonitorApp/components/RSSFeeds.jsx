451c652a678a3d799a4a5195410185f2c5a2b1a7
Sjors Provoost <sjors@sprovoost.nl>
2023-04-14 20:09:01 +0200

    Reformat RSS feed page

--- app/javascript/packs/forkMonitorApp/components/RSSFeeds.jsx
+++ app/javascript/packs/forkMonitorApp/components/RSSFeeds.jsx
@@ -12,120 +12,98 @@ class RSSFeeds extends React.Component {
 
         <p>You can setup notifications for the RSS feeds below using a service like <a href="https://ifttt.com">IFTTT</a>.</p>
 
-        <h3>Invalid Blocks</h3>
-
         <ul>
-          <li><a href="/feeds/btc/blocks/invalid.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
+          <li>
+          <a href="/feeds/btc/blocks/invalid.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+            <b>Invalid Blocks</b>
+            <p>
+              Fork Monitor stores all invalid chaintip blocks in its own database. Invalid
+              blocks are occasionally found and don't have to be a problem (except
+              for the miner), as long as all nodes agree they're invalid.
+            </p>
+          </li>
+          <li>
+            <a href="/feeds/invalid_blocks/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+            <b>Inconsistent validity (blocks)</b>
+            <p>
+              Invalid blocks are occasionally found and don't have to be a problem (except
+              for the miner), as long as all nodes agree they're invalid. But when some
+              nodes consider a block valid, while others consider it invalid, this
+              inconsistent validity could indicate a consensus bug.
+            </p>
+
+            <p>
+              Fork Monitor stores both valid and invalid blocks in its own database,
+              including intermediate blocks between poll moments, and including `valid-fork`
+              entries from `getchaintips`. It then takes the `invalid` chaintip entries
+              from each node and checks if any other node considered it valid. If so
+              then it sends out an alert.
+            </p>
+          </li>
+          <li>
+            <a href="/feeds/inflated_blocks/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+            <b>Inflated Blocks</b>
+            <p>
+              Creates an alert if a block increases the total supply by more than 12.5 (later: 6.25, etc) BTC.
+              This is checked using `gettxoutsetinfo`.
+            </p>
+          </li>
+          <li>
+            <a href="/feeds/stale_candidates/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+            <b>Stale block candidates</b>
+            <p>
+              Creates an alert if there is more than one block at the tip height of the chain.
+              E.g. if there are two blocks at height N then one is expected to become stale.
+
+              This will not create an alert for all stale blocks, only those that have been
+              processed by our nodes.
+            </p>
+          </li>
+          <li>
+            <a href="/feeds/blocks/unknown_pools/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+            <b>Unidentified mining pools</b>
+            <p>
+              List of recent blocks mined by a pool that can't be matched <a href="https://github.com/BitMEXResearch/forkmonitor/blob/master/app/models/block.rb#L158">by this list</a>
+              </p>
+          </li>
+          <li>
+            <a href="/feeds/version_bits.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+            <b>Version bits signalling</b>
+            <p>
+              Version bits flagged in the past 100 blocks (currently uses 10 as lower threshold).
+            </p>
+          </li>
+          <li>
+            <a href="/feeds/lagging_nodes.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+            <b>Unreachable nodes</b>
+            <p>
+              Checks if any of the nodes fell behind. This doesn't have to be
+              a consensus problem, but we filter common reasons like being offline,
+              in initial block download or not having peers.
+            </p>
+          </li>
+          <li>
+            <a href="/feeds/nodes/unreachable.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+            <b>Unreachable nodes</b>
+            <p>This includes mirror nodes, which are used for inflation checks.</p>
+          </li>
+          <li>
+            <b>Lightning transactions</b>
+            <p>
+              See <a href="https://blog.bitmex.com/lightning-network-justice/">Lightning Network (Part 3) – Where Is The Justice?</a> for background.
+            </p>
+            <ul>
+              <li>
+                <a href="/feeds/ln_penalties/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+                <b>Penalties</b>
+              </li>
+              <li>
+                <a href="/feeds/ln_sweeps/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a>&nbsp;
+                <b>Delayed sweeps</b>
+              </li>
+            </ul>
+          </li>
         </ul>
-
-        <p>
-          Fork Monitor stores all invalid chaintip blocks in its own database. Invalid
-          blocks are occasionally found and don't have to be a problem (except
-          for the miner), as long as all nodes agree they're invalid.
-        </p>
-
-        <h3>Inconsistent validity (blocks)</h3>
-
-        <ul>
-          <li><a href="/feeds/invalid_blocks/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
-        <p>
-          Invalid blocks are occasionally found and don't have to be a problem (except
-          for the miner), as long as all nodes agree they're invalid. But when some
-          nodes consider a block valid, while others consider it invalid, this
-          inconsistent validity could indicate a consensus bug.
-        </p>
-
-        <p>
-          Fork Monitor stores both valid and invalid blocks in its own database,
-          including intermediate blocks between poll moments, and including `valid-fork`
-          entries from `getchaintips`. It then takes the `invalid` chaintip entries
-          from each node and checks if any other node considered it valid. If so
-          then it sends out an alert.
-        </p>
-
-        <h3>Inflated Blocks</h3>
-
-        <ul>
-          <li><a href="/feeds/inflated_blocks/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
-        <p>
-          Creates an alert if a block increases the total supply by more than 12.5 (later: 6.25, etc) BTC.
-          This is checked using `gettxoutsetinfo`.
-        </p>
-
-        <h3>Stale block candidates</h3>
-
-        <ul>
-          <li><a href="/feeds/stale_candidates/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
-        <p>
-          Creates an alert if there is more than one block at the tip height of the chain.
-          E.g. if there are two blocks at height N then one is expected to become stale.
-
-          This will not create an alert for all stale blocks, only those that have been
-          processed by our nodes.
-        </p>
-
-        <h3>Unidentified mining pools</h3>
-
-        <ul>
-          <li><a href="/feeds/blocks/unknown_pools/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
-        <p>
-          List of recent blocks mined by a pool that can't be matched <a href="https://github.com/BitMEXResearch/forkmonitor/blob/master/app/models/block.rb#L158">by this list</a>.
-        </p>
-
-        <h3>Version bit signaling</h3>
-
-        <ul>
-          <li><a href="/feeds/version_bits.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
-        <p>Version bits flagged in the past 100 blocks (currently uses 10 as lower threshold).</p>
-
-        <h3>Lagging nodes</h3>
-
-        <ul>
-          <li>coing<a href="/feeds/lagging_nodes.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
-        <p>
-          Checks if any of the nodes fell behind. This doesn't have to be
-          a consensus problem, but we filter common reasons like being offline,
-          in initial block download or not having peers.
-        </p>
-
-        <h3>Unreachable nodes</h3>
-
-        <p>This includes mirror nodes, which are used for inflation checks.</p>
-
-        <ul>
-          <li><a href="/feeds/nodes/unreachable.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
-        <h3>Lightning transactions</h3>
-
-        <p>
-          See <a href="https://blog.bitmex.com/lightning-network-justice/">Lightning Network (Part 3) – Where Is The Justice?</a> for background.
-        </p>
-
-        <b>Penalties</b>
-
-        <ul>
-          <li><a href="/feeds/ln_penalties/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
-        <b>Delayed sweeps</b>
-
-        <ul>
-          <li><a href="/feeds/ln_sweeps/btc.rss" target="_blank"><FontAwesomeIcon icon={faRss} /></a></li>
-        </ul>
-
       </div>
     )
   }
