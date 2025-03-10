1d7e6a5fc629538f2dadbe78d892aa700f4dc08e
Sjors Provoost <sjors@sprovoost.nl>
2025-03-10 11:54:22 +0100

    Bump capistrano
    Fixes SSH deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.17.0'
+lock '~> 3.19.2'
 
 set :application, 'forkmonitor'
 set :repo_url, 'https://github.com/BitMEXResearch/forkmonitor.git'
