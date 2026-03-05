1faa3579f0fac8a4ac984442427ce6c7dbaad581
Jonathan Bier <42411042+jonathanbier@users.noreply.github.com>
2026-03-05 13:08:49 +0000

    Implement country code to flag emoji conversion
    Add method to convert country code to flag emoji.

--- app/javascript/packs/forkMonitorApp/components/nodeName.jsx
+++ app/javascript/packs/forkMonitorApp/components/nodeName.jsx
@@ -1,9 +1,24 @@
+
 import React from 'react';
 
 class NodeName extends React.Component {
+  // Convert country code to flag emoji
+  countryCodeToFlag(countryCode) {
+    if (!countryCode || countryCode.length !== 2) {
+      return '';
+    }
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char => 127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  }
+
   render() {
+    const flag = this.countryCodeToFlag(this.props.node.country);
     return(
       <span>
+        {flag && <span title={this.props.node.country}>{flag}&nbsp;</span>}
         {this.props.node.name_with_version}
       </span>
     )
